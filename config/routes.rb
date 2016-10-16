ddc98ac97e68cac428897b59d5ddc9f9e00d1979
Anthony J. Rodarte <anthonyrodarte22@yahoo.com>
2016-10-15 17:21:54 -0700

    Added a home page and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact-us' => 'pages#contact-us'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
