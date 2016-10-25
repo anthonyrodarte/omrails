c47c47440a50bac0ec1e05033ab1b6115c6c1613
Anthony J. Rodarte <anthonyrodarte22@yahoo.com>
2016-10-24 17:41:37 -0700

    Add Users with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  as :user do
+      get 'sign_in' => 'devise/sessions#new'
+      delete 'sign_out' => 'devise/sessions#destroy'
+      get 'sign_up' => 'devise/registrations#new'
+  end
+  
   root 'pages#home'
   get 'about' => 'pages#about'
-  get 'contactus' => 'pages#contactus'
+  get 'contact_us' => 'pages#contactus'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
