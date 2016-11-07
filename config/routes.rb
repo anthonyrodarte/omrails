cc31ccb40645f6c4956c5647b1e797ac5bc48968
Anthony J. Rodarte <anthonyrodarte22@yahoo.com>
2016-11-06 18:01:48 -0800

    Add following and feed functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/:username', to: 'users#show', as: 'user'
+  get 'feed', to: 'feed#show'
 
+  resources :users, only: :show, param: :username do
+    member do
+      post 'follow', to: 'follows#create'
+      delete 'unfollow', to: 'follows#destroy'
+    end
+  end
   resources :reviews
   resources :tweets
   ActiveAdmin.routes(self)
@@ -10,10 +16,8 @@ Rails.application.routes.draw do
       delete 'sign_out', to: 'devise/sessions#destroy'
       get 'sign_up', to: 'devise/registrations#new'
   end
-  
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact_us', to: 'pages#contactus'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
